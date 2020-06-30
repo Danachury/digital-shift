89f69276d7f06f325eb91c480893ddcc91078ba9
Denilson Anachury <danachury@easysol.net>
2020-06-30 12:14:01 -0500

    Added .dev/ folder

---
+++ .dev/asciinema.sh
@@ -0,0 +1,6 @@
+asciinema rec Documents/add_network.json
+asciinema rec Documents/add_volumes.json
+asciinema rec Documents/start_jenkins.json
+asciinema rec Documents/start_jenkins_blueocean.json
+
+asciicast2gif -s 2 -S 1 ../start_container ../start_container.gif
