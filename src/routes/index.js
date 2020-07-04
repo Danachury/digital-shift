9b81dd9a539f8c8c22c974ae444b924dbc18f77b
Denilson Anachury <danachury@easysol.net>
2020-07-04 11:12:35 -0500

    Add unit tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../models/user');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-   res.render('index', { message: 'Hello Digital Shift!' });
+   res.render('index', { message: `Hello ${user.formattedName()}!` });
 });
 
 /* GET 404 Error. */
 router.get('/page-not-found', (req, res) => {
-   res.render('error', { message: '404 Page not found'});
+   res.render('error', { message: '404 Page not found' });
 });
 
 module.exports = router;
