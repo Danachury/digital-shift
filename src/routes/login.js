b3d321874c9942dfd37f6040f4daa9bc19a6d427
Denilson Anachury <danachury@easysol.net>
2020-07-05 12:47:56 -0500

    Add login route

---
+++ src/routes/login.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * Page request handler.
+ */
+router.get('/', (req, res) => {
+   res.render('login');
+});
+
+/**
+ * Form submit handler.
+ */
+router.post('/', (req, res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+
+   if (username && password) {
+      res.redirect(`home?username=${username}`);
+   } else {
+      res.render('login', { loginError: 'Invalid Username and Password combination.' });
+   }
+   res.end();
+});
+
+module.exports = router;
